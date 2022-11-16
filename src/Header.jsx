fe8ca0eb44b708db97987bd7250da32f6883da4e
ÐMaria Korol <KorolMV@eurotorg.by>
2022-11-16 16:06:06 +0300

    feat: add client config

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header>
+            <h1>MODERN Chat</h1>
+        </header>
+    )
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Header } from './Header';
+
+window.addEventListener('load', () => {
+    ReactDOM.render(<Header/>, document.getElementById("root"));
+})
+
+
+
