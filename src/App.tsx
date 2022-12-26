4ffb3cd92281e1e8972bbb098e1af4161dcb8ab4
ÐMaria Korol <KorolMV@eurotorg.by>
2022-12-26 10:22:04 +0300

    feat: card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {hot} from "react-hot-loader/root";
-
-import './main.global.less';
 import {CardsList} from "./shared/CardsList";
 import {Layout} from "./shared/Layout";
 import {Content} from "./shared/Content";
 import {Header} from "./shared/Header";
+import './main.global.less';
 
 function AppComponent () {
     return (
